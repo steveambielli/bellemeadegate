0919d3233ac0a1143e39dc40b0c8241f238b9afe
Steve Ambielli <steve.ambielli@gmail.com>
2023-10-17 12:07:28 -0400

    removed fetch; added mock data

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,22 @@
 async function getData() {
-  const res = await fetch("http://127.0.0.1:3000/api", { cache: "no-store" })
+  /*const res = await fetch("http://127.0.0.1:3000/api", { cache: "no-store" })
   if (!res.ok) {
     throw new Error("Failed to fetch data")
   }
-  //TODO add logic here to set days and colors
-  return res.json()
-}
-
-const statusColor = (date: string, status: string) => {
-  if (date) {
-    if (daysDiff(date) === 0) {
-      return "red"
-    } else {
-      return "green"
-    }
-  }
-  if (status === "Active") {
-    return "green"
-  } else {
-    return "red"
+  return res.json()*/
+  return {
+    data: {
+      mainGate: {
+        status: "Active",
+        arm: "Active",
+        lastIncident: "2023-10-16",
+      },
+      residentGate: {
+        status: "Active",
+        arm: "Active",
+        lastIncident: "2022-10-01",
+      },
+    },
   }
 }
 
@@ -38,34 +36,34 @@ export default async function Home() {
       <h1 className="text-5xl font-black mb-4">Belle Meade Gate</h1>
       <h3 className="mt-5 font-bold text-4xl">Main Gate</h3>
       <div className="flex flex-row mt-4">
-        <span
-          className={`rounded font-bold bg-${statusColor(
-            "",
-            data.data.mainGate.status
-          )}-500 p-1 px-4`}
-        >
-          Gate: {data.data.mainGate.status}
-        </span>
-        <span
-          className={`rounded font-bold bg-${statusColor(
-            "",
-            data.data.mainGate.arm
-          )}-500 p-1 px-4 mx-2`}
-        >
-          Arm: {data.data.mainGate.arm}
-        </span>
+        {data.data.mainGate.status === "Active" && (
+          <span className={`rounded font-bold bg-green-500 p-1 px-4 mx-2`}>
+            Gate: {data.data.mainGate.status}
+          </span>
+        )}
+        {data.data.mainGate.status === "Down" && (
+          <span className={`rounded font-bold bg-red-500 p-1 px-4 mx-2`}>
+            Gate: {data.data.mainGate.status}
+          </span>
+        )}
+
+        {data.data.mainGate.arm === "Active" && (
+          <span className={`rounded font-bold bg-green-500 p-1 px-4 mx-2`}>
+            Arm: {data.data.mainGate.arm}
+          </span>
+        )}
+        {data.data.mainGate.arm === "Down" && (
+          <span className={`rounded font-bold bg-red-500 p-1 px-4 mx-2`}>
+            Arm: {data.data.mainGate.arm}
+          </span>
+        )}
       </div>
       <div className="flex flex-row gap-10 text-center">
         <div>
           <h3 className="mt-5 font-bold text-1xl">
             Days since the last incident:
           </h3>
-          <div
-            className={`mt-3 rounded font-bold bg-${statusColor(
-              data.data.mainGate.lastIncident,
-              ""
-            )}-700 p-12 text-6xl`}
-          >
+          <div className={`mt-3 rounded font-bold bg-blue-700 p-12 text-6xl`}>
             {daysDiff(data.data.mainGate.lastIncident)}
           </div>
         </div>
@@ -73,34 +71,34 @@ export default async function Home() {
 
       <h3 className="mt-10 font-bold text-4xl">Resident Gate</h3>
       <div className="flex flex-row mt-4">
-        <span
-          className={`rounded font-bold bg-${statusColor(
-            "",
-            data.data.residentGate.status
-          )}-500 p-1 px-4`}
-        >
-          Gate: {data.data.residentGate.status}
-        </span>
-        <span
-          className={`rounded font-bold bg-${statusColor(
-            "",
-            data.data.residentGate.arm
-          )}-500 p-1 px-4 mx-2`}
-        >
-          Arm: {data.data.residentGate.arm}
-        </span>
+        {data.data.residentGate.status === "Active" && (
+          <span className={`rounded font-bold bg-green-500 p-1 px-4 mx-2`}>
+            Gate: {data.data.residentGate.status}
+          </span>
+        )}
+        {data.data.residentGate.status === "Down" && (
+          <span className={`rounded font-bold bg-red-500 p-1 px-4 mx-2`}>
+            Gate: {data.data.residentGate.status}
+          </span>
+        )}
+
+        {data.data.residentGate.arm === "Active" && (
+          <span className={`rounded font-bold bg-green-500 p-1 px-4 mx-2`}>
+            Arm: {data.data.residentGate.arm}
+          </span>
+        )}
+        {data.data.residentGate.arm === "Down" && (
+          <span className={`rounded font-bold bg-red-500 p-1 px-4 mx-2`}>
+            Arm: {data.data.residentGate.arm}
+          </span>
+        )}
       </div>
       <div className="flex flex-row gap-10 text-center">
         <div>
           <h3 className="mt-5 font-bold text-1xl">
             Days since the last incident:
           </h3>
-          <div
-            className={`mt-3 rounded font-bold bg-${statusColor(
-              data.data.residentGate.lastIncident,
-              ""
-            )}-700 p-12 text-6xl`}
-          >
+          <div className={`mt-3 rounded font-bold bg-blue-700 p-12 text-6xl`}>
             {daysDiff(data.data.residentGate.lastIncident)}
           </div>
         </div>
